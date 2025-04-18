8406d31a49628fae84d88d0155a2dc0b47322fa6
Serhii Vovchenko <serega.vovchenko@gmail.com>
2025-04-18 12:14:28 +0200

    initial commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import s from './Footer.module.css';
+
+const Footer = () => {
+	return (
+		<div>
+			<p>Footer</p>
+		</div>
+	);
+};
+
+export default Footer;
---
+++ src/pages/CarrierProfile/CarrierProfilePage.jsx
@@ -0,0 +1,11 @@
+import s from './CarrierProfilePage.module.css';
+
+const CarrierProfilePage = () => {
+	return (
+		<div>
+			<p>CarrierProfilePage</p>
+		</div>
+	);
+};
+
+export default CarrierProfilePage;
